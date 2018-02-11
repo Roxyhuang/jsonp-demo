2d94f29709e9961b883f87718dd5dca285b66f5e
roxy_huang <sixin_huang@outlook.com>
2018-02-11 14:05:14 +0800

    update xss

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 const Koa = require('koa');
+const xss = require('xss');
 const app = new Koa();
 
 app.use(async ctx => {
@@ -8,7 +9,9 @@ app.use(async ctx => {
   const data = { status: '1000', url: 'http://www.baidu.com' };
   if(req.query && req.query.callback) {
     const str =  req.query.callback + '(' + JSON.stringify(data) + ')'; //jsonp
-    ctx.body = str;
+    // 此处增加xss过滤，若没有xss过滤 访问 http://localhost:5000/?callback=%3Cscript%3Ealert(123)%3C/script%3E
+    // 会出现xss问题
+    ctx.body = xss(str);
 
   } else {
     ctx.body = JSON.stringify(data);
