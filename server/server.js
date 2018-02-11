4615d621391f0af6b9f5fb1ba53641a0a436d344
roxy_huang <sixin_huang@outlook.com>
2018-02-11 16:31:15 +0800

    update MIME-sniffing

--- server/server.js
+++ server/server.js
@@ -8,9 +8,10 @@ app.use(async ctx => {
   console.log(req.query);
   const data = { status: '1000', url: 'http://www.baidu.com' };
   if(req.query && req.query.callback) {
-    const str =  req.query.callback + '(' + JSON.stringify(data) + ')'; //jsonp
+    ctx.set('X-Content-Type-Options' , 'nosniff'); // 如果有设置设置阻止浏览器 MIME-sniffing，需要设置ctx.type
+    ctx.type = 'text/javascript; charset=utf-8';
+    const str =  req.query.callback + '(' + JSON.stringify(data) + ')'; // jsonp
     // 此处增加xss过滤，若没有xss过滤 访问 http://localhost:5000/?callback=%3Cscript%3Ealert(123)%3C/script%3E
-    // 会出现xss问题
     ctx.body = xss(str);
 
   } else {
